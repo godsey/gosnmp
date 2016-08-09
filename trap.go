4c378e761050690daa5ea7146869a6a150cc2c86
Sonia Hamilton <sonia@snowfrog.net>
2016-08-09 22:32:26 +1000

    start on code structure for release candidate
    Ignore K's contributions, write sending code from scratch. Receiving
    code also needs tests.

--- trap.go
+++ trap.go
@@ -9,6 +9,56 @@ import (
 	"net"
 )
 
+/***************************************************************
+SNMP Traps
+
+Most SNMP PDUType's (eg GetRequest, SetRequest) are synchronous ie
+a "question" is sent and a "reply" is expected. Traps are
+asynchronous ie an unsolicited "notification" is sent from an
+Agent, without expectation that the "notification" will be received
+by a Network Management System (NMS). Therefore trap code is in
+this separate file.
+
+From stackoverflow:
+
+1. SNMP v1 defines a special TRAP message format, different from other
+messages (such as GET).  http://tools.ietf.org/html/rfc1157#page-27
+This message format is not used anymore in SNMP v2 and v3. If an SNMP
+agent sends out such TRAP messages for v2 or v3, that should be a bug.
+(... I know that some devices send the old format for SNMPv2 ... but
+that should be treated as a bug in the firmware).
+
+2. Since v2, TRAP starts to use the common message format (the same
+as GET and so on). So it is called SNMPv2-Trap-PDU.
+http://tools.ietf.org/search/rfc3416#page-22
+
+3. SNMP v3 introduces the security model to all messages, so TRAP
+receives such update too. It is still based on SNMPv2-Trap-PDU.
+
+From O'Reilly Essential SNMP:
+
+SNMPv2 defines traps in a slightly different way. In a MIB, Version 1
+traps are defined as TRAP-TYPE, while Version 2 traps are defined as
+NOTIFICATION-TYPE.  SNMPv2 also does away with the notion of generic
+traps -- instead, it defines many specific traps (properly speaking,
+notifications) in public MIBs. SNMPv3 traps, which are discussed
+briefly in Appendix F, "SNMPv3", are simply SNMPv2 traps with added
+authentication and privacy capabilities. Most SNMP implementations
+support only Version 1.
+***************************************************************/
+
+//
+// Sending Traps ie GoSNMP acting as an Agent
+//
+
+// TODO...
+
+//
+// Receiving Traps ie GoSNMP acting as an NMS
+//
+// GoSNMP.unmarshal() currently only handles SNMPV2Trap
+//
+
 // A TrapListener defineds parameters for running a SNMP Trap receiver.
 // nil values will be replaced by default values.
 type TrapListener struct {
